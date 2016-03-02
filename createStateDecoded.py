0e91d401848918043b23a3921de54c0eda618d00
Doug Turnbull <softwaredoug@gmail.com>
2016-03-02 14:34:12 -0500

    Add script to create statedecoded index

---
+++ createStateDecoded.py
@@ -0,0 +1,29 @@
+
+
+settings = {
+    "mappings": {
+      "law": {
+        "properties": {
+            "text": {
+              "type": "string",
+              "term_vector": "with_positions_offsets_payloads"
+            }
+        }
+      }
+    },
+    "settings": {
+      "index" : {
+        "number_of_shards" : 1,
+        "number_of_replicas" : 0
+      },
+    }
+}
+
+from elasticsearch import Elasticsearch, TransportError
+
+try:
+    es = Elasticsearch("http://localhost:9200")
+    es.indices.delete(index='statedecoded', ignore=[400,404])
+    es.indices.create(index='statedecoded', body=settings)
+except TransportError as e:
+    print(repr(e))
